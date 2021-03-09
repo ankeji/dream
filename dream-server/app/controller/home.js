3315bf2cf2725ac1142a5e4c4e145c5303ed9140
ankeji <ankeji1995@163.com>
2021-03-09 10:12:26 +0800

    第一次提交

--- dream-server/app/controller/home.js
+++ dream-server/app/controller/home.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ankeji
+ * @Date: 2021-03-09 09:47:08
+ * @LastEditors: ankeji
+ * @LastEditTime: 2021-03-09 10:08:49
+ */
 'use strict';
 
 const Controller = require('egg').Controller;
@@ -10,3 +18,4 @@ class HomeController extends Controller {
 }
 
 module.exports = HomeController;
+
